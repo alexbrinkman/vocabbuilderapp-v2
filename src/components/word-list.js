4d0a8dd30f51bf14499c2f68fa18e7bca8baac19
Alex Brinkman <alex@enlightenedpixel.com>
2023-06-25 00:12:08 -0600

    Update word-list.js

--- src/components/word-list.js
+++ src/components/word-list.js
@@ -12,274 +12,211 @@ const WordList = [
       },
       {
         id: 1,
-        word: "olive oil",
-        definition: "aceite de oliva",
-        complete: false,
-      },
-      {
-        id: 2,
-        word: "not yet",
-        definition:
-          "todavía no",
-        complete: false,
-      },
-      {
-        id: 3,
-        word: "shoulder",
-        definition: "el hombro",
-        complete: false,
-      },
-      {
-        id: 4,
-        word: "lifting weights",
-        definition: "levantando pesas",
-        complete: false,
-      },
-      {
-        id: 5,
-        word: "I like the shade",
-        definition: "me gusta la sombra",
-        complete: false,
-      },
-      {
-        id: 6,
-        word: "most people",
-        definition: "la mayoría de las personas",
-        complete: false,
-      },
-      {
-        id: 7,
         word: "I'm trying to learn Spanish because I want to teach my son",
         definition: "Estoy tratando de aprender español porque quiero enseñarle a mi hijo.",
         complete: false,
       },
       {
-        id: 8,
+        id: 2,
         word: "I hung out with my buddy this weekend.  We went to a bar and drank some beers and talked in Spanish.",
         definition: "Salí con mi amigo este fin de semana. Fuimos a un bar y bebimos unas cervezas y hablamos en español.",
         complete: false,
       },
       {
-        id: 9,
+        id: 3,
         word: "Do you ever get tired of talking all day",
         definition: "¿Alguna vez te cansas de hablar todo el día?",
         complete: false,
       },
       {
-        id: 10,
+        id: 4,
         word: "What do you call it?",
         definition:
           "Cómo lo llamas?",
         complete: false,
       },
       {
-        id: 11,
+        id: 5,
         word: "Have a nice day!",
         definition:
           "Que tengas un lindo dia!",
         complete: false,
       },
       {
-        id: 12,
+        id: 6,
         word: "What did you do today?",
         definition: "Qué hiciste hoy?",
         complete: false,
       },
       {
-        id: 13,
+        id: 7,
         word: "I have been listening to Spanish all weekend and I'm getting much better.",
         definition: "He estado escuchando español todo el fin de semana y estoy mejorando mucho.",
         complete: false,
       },
       {
-        id: 14,
+        id: 8,
         word: "I listened to the podcast about the man from Leadville and I understood almost all of it.",
         definition: "Escuché el podcast sobre el chico de Leadville y entendí casi todo.",
         complete: false,
       },
       {
-        id: 15,
+        id: 9,
         word: "There weren't any",
         definition: "No hubo ninguna",
         complete: false,
       },
       {
-        id: 16,
-        word: "never the less",
-        definition:
-          "sin embargo",
-        complete: false,
-      },
-      {
-        id: 17,
+        id: 10,
         word: "I had",
         definition:
           "tuve",
         complete: false,
       },
       {
-        id: 18,
+        id: 11,
         word: "When I was a kid, we went to Mexico often.",
         definition:
           "Cuando era niño, íbamos a México a menudo.",
         complete: false,
       },
       {
-        id: 19,
+        id: 12,
         word: "I'm not going to be able to explain it.",
         definition:
           "No voy a poder explicarlo.",
         complete: false,
       },
+     
       {
-        id: 20,
-        word: "Software engineer",
-        definition:
-          "Ingeniero de software",
-        complete: false,
-      },
-      {
-        id: 21,
+        id: 13,
         word: "If you are a realator and you need someone to show a home for you, you can put it on our site and someone will show it for you",
         definition:
           "Si eres un realator y necesita que alguien le muestre una casa, puede ponerla casa en nuestro sitio web y alguien más se la mostrará",
         complete: false,
       },
       {
-        id: 22,
+        id: 14,
         word: "I worked with a gal that wanted to learn a Colombian accent.",
         definition:
           "Trabajé con una chica que quería aprender un acento colombiano.",
         complete: false,
       },
       {
-        id: 23,
+        id: 15,
         word: "Before the pandemic, there was always freedom to travel.",
         definition:
           "Antes de la pandemia, siempre había libertad para viajar.",
         complete: false,
       },
       {
-        id: 24,
-        word: "There has been a change in temperature.",
-        definition:
-          "Ha habido un cambio en la temperatura.",
-        complete: false,
-      },
-      {
-        id: 25,
+        id: 16,
         word: "There was a change in temperature.",
         definition:
           "Hubo un cambio en la temperatura.",
         complete: false,
       },
+     
       {
-        id: 26,
-        word: "there is/are",
-        definition:
-          "hay",
-        complete: false,
-      },
-      {
-        id: 27,
+        id: 17,
         word: "there was/were",
         definition:
           "hubo / había (imperfect)",
         complete: false,
       },
       {
-        id: 28,
+        id: 18,
         word: "there will be",
         definition:
           "habrá",
         complete: false,
       },
       {
-        id: 29,
+        id: 19,
         word: "there would be",
         definition:
           "habriá",
         complete: false,
       },
       {
-        id: 30,
+        id: 20,
         word: "I should have gone to the store this morning.",
         definition:
           "Yo debería haber ido a la tienda esta mañana. ",
         complete: false,
       },
       {
-        id: 31,
+        id: 21,
         word: "I have read the book.",
         definition:
-          "He leído el libro. ",
+          "He leído el libro.",
         complete: false,
       },
       {
-        id: 32,
+        id: 22,
         word: "Is anyone here?",
         definition:
           "¿Hay alguien aquí?",
         complete: false,
       },
       {
-        id: 33,
+        id: 23,
         word: "Were there any owls in the forest?",
         definition:
           "¿Había algunos búhos en el bosque?",
         complete: false,
       },
       {
-        id: 34,
+        id: 24,
         word: "I have lost my cat.",
         definition:
           "He perdido mi gato.",
         complete: false,
       },
       {
-        id: 35,
+        id: 25,
         word: "There are going to be questions.",
         definition:
           "Va a haber preguntas.",
         complete: false,
       },
       {
-        id: 36,
+        id: 26,
         word: "Were there any lions at the zoo?",
         definition:
           "¿Había algunos leones en el zoológico?",
         complete: false,
       },
       {
-        id: 37,
+        id: 27,
         word: "There would be a wedding if she wanted one.",
         definition:
           "Habría una boda si ella la quisiera. ",
         complete: false,
       },
       {
-        id: 38,
+        id: 28,
         word: "There will be a delicious dinner on the table when she comes back.",
         definition:
           "Habrá una cena deliciosa en la mesa cuando ella vuelva.",
         complete: false,
       },
       {
-        id: 39,
+        id: 29,
         word: "It wasn't me!",
         definition:
           "Yo no he sido!",
         complete: false,
       },
       {
-        id: 40,
+        id: 30,
         word: "It was him!",
         definition:
           "Ha sido el",
         complete: false,
       },
       {
-        id: 41,
+        id: 31,
         word: "What do you do for work?",
         definition:
           "¿A qué te dedicas? o ¿En qué trabajas?",
